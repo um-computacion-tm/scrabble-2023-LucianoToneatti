1b98122ecbdab3a580937b84c0851ccddf00a3e3
LucianoToneatti <l.toneatti@alumno.um.edu.ar>
2023-08-29 18:32:31 -0300

    Creacion de libreria6

---
+++ Test/test_dictionary.py
@@ -0,0 +1,9 @@
+import unittest
+from game.dictionary import Dictionary
+from game.board import Board
+
+
+class TestDictionary(unittest.TestCase):
+    def test_dictionary_true(self):
+
+        dictionary = Dictionary()
