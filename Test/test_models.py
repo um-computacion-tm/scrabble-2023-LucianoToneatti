a31dc345cb4df3cc66e03e876d41f7f22a686e21
LucianoToneatti <l.toneatti@alumno.um.edu.ar>
2023-08-28 23:08:31 -0300

    2doCommit

--- Test/test_models.py
+++ Test/test_models.py
@@ -1,7 +1,10 @@
-import unittest 
+import unittest
+from game.models import (
+    BagTiles,
+    Tile,
+)
 from unittest.mock import patch
-from game.models import Tile 
-from game.models import BagTiles
+
 
 class TestTiles(unittest.TestCase):
     def test_tile(self):
@@ -11,23 +14,43 @@ class TestTiles(unittest.TestCase):
 
 class TestBagTiles(unittest.TestCase):
     @patch('random.shuffle')
-    def test_bag_tiles(self, patch_shuffle ):
+    def test_bag_tiles(self, patch_shuffle):
         bag = BagTiles()
-        self.assertEqual(len(bag.tiles),5,)
-        self.assertEqual(patch_shuffle.call_count,1,)
-        self.assertEqual(patch_shuffle.call_args [0][0],bag.tiles,)
-        
+        self.assertEqual(
+            len(bag.tiles),
+            5,
+        )
+        self.assertEqual(
+            patch_shuffle.call_count,
+            1,
+        )
+        self.assertEqual(
+            patch_shuffle.call_args[0][0],
+            bag.tiles,
+        )
+
 
     def test_take(self):
         bag = BagTiles()
         tiles = bag.take(2)
-        self.assertEqual(len(bag.tiles), 3,)
-        self.assertEqual(len(tiles), 2,)
+        self.assertEqual(
+            len(bag.tiles),
+            3,
+        )
+        self.assertEqual(
+            len(tiles),
+            2,
+        )
 
     def test_put(self):
         bag = BagTiles()
-        put_tiles = [Tile('A', 1), Tile('Y',1)]
+        put_tiles = [Tile('Z', 1), Tile('Y', 1)]
         bag.put(put_tiles)
+        self.assertEqual(
+            len(bag.tiles),
+            7,
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
