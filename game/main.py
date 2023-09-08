abb085c4ef236cb139b4ccb6adf544112758f1d7
LucianoToneatti <l.toneatti@alumno.um.edu.ar>
2023-09-07 23:30:14 -0300

    Se agregaron turnos 7

--- game/main.py
+++
@@ -1,7 +0,0 @@
-from game.scrabble import Scrabble
-
-
-def main():
-    player_count = int(input('cantidad de jugadores'))
-    game = Scrabble(player_count)
-    print('La cantidad de jugadores es: ' + str(player_count))
