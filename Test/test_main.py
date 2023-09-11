04c825b708e13c80e534e4df054f59673a82d739
LucianoToneatti <l.toneatti@alumno.um.edu.ar>
2023-09-10 21:06:05 -0300

    calculate_word_value 10

--- Test/test_main.py
+++ Test/test_main.py
@@ -1,14 +1,22 @@
+from main import main
 import unittest
 from unittest.mock import patch
 from io import StringIO
-from main import ScrabbleGame
-class TestScrabbleGame(unittest.TestCase):    
-    @patch('builtins.input', side_effect=['3'])
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_scrabble_game_with_valid_input(self, mock_stdout, mock_input):
-        game = ScrabbleGame()
-        game.run()
-        expected_output = 'Bienvenido\nLa cantidad de jugadores es: 3\nTurno del jugador 1\n'
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+
+class TestScrabbleGame(unittest.TestCase):
+    @patch('builtins.input', side_effect=['3']) 
+    def test_player_count_input_valid(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            main()
+            self.assertEqual(mock_stdout.getvalue(), 'Bienvenido\nLa cantidad de jugadores es: 3\nTurno del jugador 1\n')
+
+    @patch('builtins.input', side_effect=['5', '3'])
+    def test_player_count_input_invalid_then_valid(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            main()
+            expected_output = 'Bienvenido\nValor invalido\nLa cantidad de jugadores es: 3\nTurno del jugador 1\n'
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
+
 if __name__ == '__main__':
     unittest.main()
--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 from game.scrabble import Scrabble
 
-class ScrabbleGame: 
 
-    def __init__(self):
-        self.player_count = self.get_valid_player_count()    
-    def get_valid_player_count(self):
-            while True:
-                player_count = self.get_player_input()
-                if 2 <= player_count <= 4:
-                    return player_count
-                print('Valor inválido. Debe ser un número entre 2 y 4.')    
-    def get_player_input(self):
+def main():
+    print('Bienvenido')
+    while True:
         try:
-            return int(input('Cantidad de jugadores: '))
+            player_count = int(input('cantidad de jugadores '))
+            if player_count < 2 or player_count > 4:
+                raise ValueError
+            else:
+                break
         except ValueError:
-            print('Valor inválido. Debe ser un número.')    
-    def welcome_message(self):
-        print('Bienvenido')    
-    def display_player_count(self):
-        print(f'La cantidad de jugadores es: {self.player_count}')    
-    def next_player_turn(self, player_number):
-        print(f"Turno del jugador {player_number}")    
-    def run(self):
-        self.welcome_message()
-        self.display_player_count()        
-        game = Scrabble(self.player_count)
-        self.next_player_turn(1)
+            print('Valor invalido')
+    game = Scrabble(player_count)
+    print('La cantidad de jugadores es: ' + str(player_count))
+    game.next_turn()
+    print(f"Turno del jugador 1")
+    """word = input('Ingrese palabra: ')
+    location_x = input('Ingrese posición X: ')
+    location_y = input('Ingrese posición Y: ')
+    location = (location_x, location_y)
+    orientation = input('Ingrese orientación (V/H): ')
+    game.validate_word"""
 if __name__ == "__main__":
-    scrabble_game = ScrabbleGame()
-    scrabble_game.run()
+    main()
+    
